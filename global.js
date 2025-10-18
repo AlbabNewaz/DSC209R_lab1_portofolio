2fcfeac724a13b48f6ddac97175a9992bb94c72f
Albab Neawz <newaz.albab@gmail.com>
2025-10-17 20:11:22 -0700

    test

--- global.js
+++ global.js
@@ -1,13 +1,75 @@
-console.log("IT’S ALIVE!");
+console.log("IT'S ALIVE!");
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+const pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'Contact/', title: 'Contact' },
+  { url: 'Resume/', title: 'Resume' },
+  { url: 'https://github.com/AlbabNewaz', title: 'GitHub' },
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/'
+    : '/DSC209R_lab1_portofolio/';
+
+const nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+
+  nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+<label class="color-scheme">
+  Theme:
+  <select>
+    <option value="light dark">Automatic</option>
+    <option value="light">Light</option>
+    <option value="dark">Dark</option>
+  </select>
+</label>
+`
 );
 
-currentLink?.classList.add("current");
+const select = document.querySelector('.color-scheme select');
+
+function setColorScheme(value) {
+  document.documentElement.style.setProperty('color-scheme', value);
+  select.value = value;
+  localStorage.setItem('colorScheme', value);
+}
+
+if ('colorScheme' in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener('input', (event) => {
+  setColorScheme(event.target.value);
+});
