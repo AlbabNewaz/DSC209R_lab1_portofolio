f8d03dd0dc1bc8f26e2fb56037906b25a4188e77
Albab Neawz <newaz.albab@gmail.com>
2025-10-31 20:52:12 -0700

    5.01

--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,8 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
-const titleEl = document.querySelector('.projects-title');
-if (titleEl && projects) {
+const titleEl = document.querySelector(".projects-title");
+if (titleEl) {
+  const projects = await fetchJSON("../lib/projects.json");
   titleEl.textContent = `Projects (${projects.length})`;
 }
